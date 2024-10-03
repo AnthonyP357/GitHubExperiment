525d26613b47f3d96eac9d73340220389b896635
Anthony Perez <aperez327@gmail.com>
2024-10-03 12:55:34 -0400

    Initial Commit

---
+++ GitHubExperiment/GitHubExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExperimentApp.swift
+//  GitHubExperiment
+//
+//  Created by Anthony Perez on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
